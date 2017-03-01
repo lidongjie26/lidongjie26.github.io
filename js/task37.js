33ef4018931c276a3d3a32b62789902f9a9cc83f
李冬杰 <lidongjie26@outlook.com>
2017-03-01 15:33:07 +0800

    update

--- js/task37.js
+++ js/task37.js
@@ -1,17 +1,9 @@
-/**
- * Created by 李冬杰 on 2017/2/20.
- */
-/*
- 重构为mvc模式
-  控制器
-  模型
-  视图
- */
 
 var btnFloat=document.getElementById('float-btn');
 var btnClose=document.getElementsByClassName('close')[0];
 var floatWrap=document.getElementById('float-wrap');
 var body=document.body;
+//元素居中
 function autoCenter(el) {
     var bodyW = document.documentElement.clientWidth;
     var bodyH = document.documentElement.clientHeight;
@@ -22,7 +14,7 @@ function autoCenter(el) {
     el.style.left = (bodyW - elW)/2+ "px";
     el.style.top = (bodyH - elH)/2+ "px";
 }
-
+//制作薄膜
 function getHover(divFloat){
     var box=divFloat.parentNode;
     var height=box.clientHeight;
@@ -37,11 +29,13 @@ function getHover(divFloat){
         hover.className='noShow';
     };
 }
+//事件绑定
 btnFloat.onclick=function(){
     floatWrap.className='show';
     autoCenter(floatWrap);
     getHover(floatWrap);
 };
+
 btnClose.onclick=function(){
     floatWrap.className='noShow';
     floatWrap.previousElementSibling.className='noShow';
