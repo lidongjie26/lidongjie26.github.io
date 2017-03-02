95dc48505e3591ac08fe4508e64b41a3d5311bb7
李冬杰 <lidongjie26@outlook.com>
2017-03-02 22:54:20 +0800

    update

--- js/task38.js
+++ js/task38.js
@@ -1,7 +1,23 @@
+//兼容性事件绑定
+var util={
+    addEventHandler:function(element,type,handle){
+        if(element.addEventListener){
+            element.addEventListener(type,handle,false);
+        }else if(element.attachEvent){
+            element.attachEvent('on'+type,handle)
+        }else{
+            element['on'+type]=handle;
+        }
+    },
+    getTarget:function(e){
+        return e.target||e.srcElement;
+    }
+};
 //新建table
 function newTable(){
     var table=document.createElement('table');
     table.className='table';
+    table.innerHTML='<tr><th>姓名</th><th>语文<span class="low">▲</span><span class="high">▼</span></th><th>数学<span class="low">▲</span><span class="high">▼</span></th><th>英语<span class="low">▲</span><span class="high">▼</span></th><th>总分<span class="low">▲</span><span class="high">▼</span></th></tr>';
     document.body.appendChild(table);
     return table;
 }
@@ -9,27 +25,38 @@ function newTable(){
 function insertRow(table,arr){
     var tr=document.createElement('tr')
     var src='';
+    var count=0;
+    if(arr.length<4){
+        for(var j=0;j<4-arr.length;i++){
+            arr.push('0');
+        }
+    }else if(arr.length>4){
+        var len=arr.length;
+        for(var j=0;j<len-4;j++){
+            arr.pop();
+        }
+    }
+    console.log(arr.length);
     for(var i=0;i<arr.length;i++){
+        if(i!==0){
+            count+=parseInt(arr[i]);
+        }
         src+='<td>'+arr[i]+'</td>'
     }
+    src+='<td>'+count+'</td>';
     tr.innerHTML=src;
     table.appendChild(tr)
 }
 //将文本框中数据根据空格分割成数组
 function textSplit(str) {
-    str=str.replace(/^\s+|\s$/g,'');
-    console.log(str)
     var arr=str.split(' ');
-    console.log(arr);
-    var len=arr.length;
-    for(var i=0; i<len; i++) {
-        console.log(len);
-        if(arr[i] ==='') {
-            arr.splice(i, 1);
-            break;
+    for(var i=arr.length; i>0; i--) {
+       if(arr[i-1]=='')
+        {
+            arr.splice(i-1, 1);
+            continue;
         }
     }
-    console.log(arr);
     if(arr.length==0){
         alert('输入为空或全为空格！');
     }else{
@@ -38,9 +65,7 @@ function textSplit(str) {
 }
 //获取文本框的文本
 $("#btnInsertTable")[0].onclick=function(){
-    var arr=textSplit($('#textBox')[0].value);
     var table=newTable();
-    insertRow(table,arr);
 }
 $("#btnInsertRow")[0].onclick=function(){
     var arr=textSplit($('#textBox')[0].value);
@@ -58,4 +83,23 @@ $('#btnDeleteRow')[0].onclick=function(){
     var tr=trs[trs.length-1];
     tr.parentNode.removeChild(tr);
 }
+//排序
+function sortNumber(a,b){
+    var aNumber=parseInt(a.firstElementChild.innerHTML);
+    var bNumber=parseInt(b.firstElementChild.innerHTML);
+    return aNumber-bNumber;
+}
+var range={
+    low:function(e){
+        var target=util.getTarget(e);
+        var col=$(target.parentNode).index();
+        var table=target.parentNode.parentNode.parentNode
+        var rows=table.rows;
+        rows.sort(sortNumber);
+    },
+    high:function(){
 
+    }
+};
+util.addHandle('click',$('.low'),range.low);
+$('.low').onclick=range.low;
